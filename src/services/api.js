8b7d709ad3ee5cf1ea053156ed07e8c5ee961bb8
Ricardo-Sousa <rwmsousa@gmail.com>
2021-08-09 18:35:07 -0300

    cria pasta componentes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch(
+    'https://api.mercadolibre.com/sites/MLB/categories',
+  );
+  const result = await response.json();
+  return result;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const response = await fetch(
+    'https://api.mercadolibre.com/sites/MLB/search?category=CATEGORY_ID&q=QUERY',
+  );
+  const result = await response.json();
+  return result;
 }
