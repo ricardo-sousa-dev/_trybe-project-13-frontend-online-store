cefbacc03b505474d59cdefcae2f4f6f929650d5
Bruno Pinho <brunopinho@outlook.com>
2021-08-11 21:33:34 -0300

    feito primeira parte do requisito 8

--- src/pages/ProductCard.js
+++ src/pages/ProductCard.js
@@ -2,25 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
-  addCart() {
-  }
+  addToCart = () => {
+    const {
+      product: { title, price, thumbnail },
+    } = this.props;
+    const localStorageProducts =
+      JSON.parse(localStorage.getItem('cartProducts')) || [];
+
+    if (!localStorageProducts) {
+      const quantity = 1;
+      const productInfo = { title, price, thumbnail, quantity };
+      localStorageProducts.push(productInfo);
+      localStorage.setItem('cartProducts', '');
+      localStorage.setItem(
+        'cartProducts',
+        JSON.stringify(localStorageProducts),
+      );
+    } else {
+      const findProduct = localStorageProducts.find(
+        (product) => product.title === title,
+      );
+      if (findProduct) {
+        findProduct.quantity += 1;
+        localStorage.setItem('cartProducts', JSON.stringify(localStorageProducts));
+      } else {
+        const quantity = 1;
+        const productInfo = { title, price, thumbnail, quantity };
+        localStorageProducts.push(productInfo);
+        localStorage.setItem('cartProducts', '');
+        localStorage.setItem(
+          'cartProducts',
+          JSON.stringify(localStorageProducts),
+        );
+      }
+    }
+  };
 
   render() {
-    const { product: { title, price, thumbnail } } = this.props;
+    const {
+      product: { title, price, thumbnail },
+    } = this.props;
 
     return (
       <div className="productCard" data-testid="product">
         <p className="productCardTitle">{title}</p>
-        <img className="productCardThumbnail" src={ thumbnail } alt={ title } />
+        <img className="productCardThumbnail" src={thumbnail} alt={title} />
         <p className="productCardPrice">
           R$
           {price}
         </p>
-
         <button
           className="productCardButton"
           type="submit"
-          onClick={ this.addCart }
+          onClick={this.addToCart}
         >
           Adicionar ao carrinho
         </button>
@@ -28,7 +62,6 @@ class ProductCard extends React.Component {
     );
   }
 }
-
 ProductCard.propTypes = {
   product: PropTypes.shape({
     price: PropTypes.number,
