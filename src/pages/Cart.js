19c126e49476ff3dad7658089e9884d4ac8ba44d
Luiz Casimiro <luizcasimi@gmail.com>
2021-08-16 19:38:04 -0300

    tentativa do requisito 10 ainda

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,80 +4,74 @@ import ButtonCart from '../components/ButtonCart';
 import CartEmpt from '../components/CartEmpt';
 
 class Cart extends React.Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  //   // this.state = {
-  //   //   forceRender: false,
-  //   // };
-  //   this.handleChange = this.handleChange.bind(this);
-  // }
+    this.state = {
+      change: 0,
+      // render: false,
+    };
+  }
 
-  // handleChange(e) {
-  //   if (e.target.className === 'buttonRemoveToCart') {
-  //     // deleteProduct = (e) => {
-  //     const currentProducts = JSON.parse(localStorage.getItem('cartProducts'));
-  //     const updatedProducts = currentProducts.filter(
-  //       (product) => product.title !== e.target.id,
-  //     );
-  //     e.target.parentNode.remove();
-  //     localStorage.setItem('cartProducts', JSON.stringify(updatedProducts));
-  //     // this.setState((prevState) => ({ forceRender: !prevState.forceRender }));
-  //     // };
-  //   }
+  handleClick = (e) => {
+    const currentProducts = JSON.parse(localStorage.getItem('cartProducts'));
+    const { change } = this.state;
 
-  //   if (e.target.className === 'buttonIncreaseQuantity') {
-  //     // increaseQuantity = e => {
-  //     const currentProducts = JSON.parse(localStorage.getItem('cartProducts'));
-  //     const indexItemIncrease = currentProducts.indexOf(
-  //       currentProducts.find((product) => product.title === e.target.id),
-  //     );
-  //     currentProducts[indexItemIncrease].quantity += 1;
-  //     localStorage.setItem('cartProducts', '');
-  //     localStorage.setItem('cartProducts', JSON.stringify(currentProducts));
-  //     // this.setState((prevState) => ({ forceRender: !prevState.forceRender }));
-  //     // };
-  //   }
-  //   if (e.target.className === 'buttonDecreaseQuantity') {
-  //     // decreaseQuantity = e => {
-  //     const currentProducts = JSON.parse(localStorage.getItem('cartProducts'));
+    if (e.target.className === 'buttonRemoveFromCart') {
+      const updatedProducts = currentProducts
+        .filter((product) => product.id !== e.target.id);
 
-  //     const indexItemDecrease = currentProducts.indexOf(
-  //       currentProducts.find((product) => product.title === e.target.id),
-  //     );
-  //     if (currentProducts[indexItemDecrease].quantity > 1) {
-  //       currentProducts[indexItemDecrease].quantity -= 1;
-  //       localStorage.setItem('cartProducts', '');
-  //       localStorage.setItem('cartProducts', JSON.stringify(currentProducts));
-  //       // this.setState((prevState) => ({ forceRender: !prevState.forceRender }));
-  //     } else {
-  //       const updatedProducts = currentProducts.filter(
-  //         (product) => product.title !== e.target.id,
-  //       );
-  //       e.target.parentNode.remove();
-  //       localStorage.setItem('cartProducts', JSON.stringify(updatedProducts));
-  // this.setState((prevState) => ({ forceRender: !prevState.forceRender }));
-  // }
-  // };
-  // }
-  // this.setState((prevState) => ({ forceRender: !prevState.forceRender }));
-  // }
+      e.target.parentNode.remove();
+
+      localStorage.setItem('cartProducts', JSON.stringify(updatedProducts));
+    }
+
+    if (e.target.className === 'buttonDecreaseQuantity') {
+      const indexItemDecrease = currentProducts
+        .indexOf(currentProducts.find((product) => product.id === e.target.id));
+
+      if (currentProducts[indexItemDecrease].quantity > 1) {
+        currentProducts[indexItemDecrease].quantity -= 1;
+
+        localStorage.setItem('cartProducts', JSON.stringify(currentProducts));
+      } else {
+        const updatedProducts = currentProducts
+          .filter((product) => product.id !== e.target.id);
+
+        e.target.parentNode.remove();
+
+        localStorage.setItem('cartProducts', JSON.stringify(updatedProducts));
+      }
+    }
+
+    if (e.target.className === 'buttonIncreaseQuantity') {
+      const indexItemIncrease = currentProducts
+        .indexOf(currentProducts.find((product) => product.id === e.target.id));
+      this.setState({ change: 1 });
+      currentProducts[indexItemIncrease].quantity += change;
+      localStorage.setItem('cartProducts', JSON.stringify(currentProducts));
+    }
+    // this.setState((prevState) => ({ render: !prevState.render }));
+  }
 
   render() {
-    const localStorageProducts = JSON.parse(
-      localStorage.getItem('cartProducts'),
-    );
-    if (!localStorageProducts || localStorageProducts === []) {
+    const currentProducts = JSON.parse(localStorage.getItem('cartProducts'));
+
+    if (!currentProducts || currentProducts === []) {
       return (
+
         <div>
           <CartEmpt />
           <Link to="/">
             <i className="fas fa-undo" />
           </Link>
         </div>
+
       );
     }
+
     return (
+
       <div className="cart">
         <Link to="/">
           <i className="fas fa-undo" />
@@ -87,62 +81,76 @@ class Cart extends React.Component {
           <ButtonCart />
           <h2>Carrinho de Compras</h2>
         </main>
+
         <div className="productList">
+
           <ul className="listProductsCart">
-            {localStorageProducts.map((product) => (
-              <li className={ product.title } key={ product.title }>
-                <button
-                  id={ product.title }
-                  className="buttonRemoveToCart"
-                  type="submit"
-                  onClick={ this.handleChange }
-                >
-                  X
-                </button>
-                <img
-                  className="imgProductCart"
-                  src={ product.thumbnail }
-                  alt={ product.title }
-                />
-                <p data-testid="shopping-cart-product-name">{product.title}</p>
-                <button
-                  id={ product.title }
-                  className="buttonDecreaseQuantity"
-                  data-testid="product-decrease-quantity"
-                  type="submit"
-                  onClick={ this.handleChange }
-                >
-                  -
-                </button>
-                <p
-                  data-testid="shopping-cart-product-quantity"
-                  // value={ quantity }
-                  // onChange={ this.handleChange }
-                >
-                  {product.quantity}
-                </p>
-                <button
-                  id={ product.title }
-                  className="buttonIncreaseQuantity"
-                  data-testid="product-increase-quantity"
-                  type="submit"
-                  onClick={ this.handleChange }
-                >
-                  +
-                </button>
-                <p>
-                  R$
-                  {product.price}
-                </p>
-              </li>
-            ))}
+
+            {currentProducts
+              .map((product) => (
+
+                <li className={ product.title } key={ product.id }>
+
+                  <button
+                    id={ product.id }
+                    className="buttonRemoveFromCart"
+                    type="submit"
+                    onClick={ this.handleClick }
+                  >
+                    X
+                  </button>
+
+                  <img
+                    className="imgProductCart"
+                    src={ product.thumbnail }
+                    alt={ product.title }
+                  />
+
+                  <p data-testid="shopping-cart-product-name">{product.title}</p>
+
+                  <button
+                    id={ product.id }
+                    className="buttonDecreaseQuantity"
+                    data-testid="product-decrease-quantity"
+                    type="submit"
+                    onClick={ this.handleClick }
+                  >
+                    -
+                  </button>
+
+                  <p data-testid="shopping-cart-product-quantity">{product.quantity}</p>
+
+                  <button
+                    id={ product.id }
+                    className="buttonIncreaseQuantity"
+                    data-testid="product-increase-quantity"
+                    type="submit"
+                    onClick={ this.handleClick }
+                  >
+                    +
+                  </button>
+
+                  <p>
+                    R$
+                    {product.price}
+                  </p>
+
+                </li>
+
+              ))}
+
           </ul>
+
           <Link to="/checkout" data-testid="checkout-products">
             Finalizar compra
           </Link>
+
         </div>
+
       </div>
+
     );
   }
 }
+
 export default Cart;
